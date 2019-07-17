0051f74041d9160e3ad995784c2efaa52c31430f
FineLime <42850488+FineLime@users.noreply.github.com>
2019-07-17 20:10:41 +0100

    Update testing.py

--- commands/testing.py
+++ commands/testing.py
@@ -13,10 +13,27 @@ class Testing(commands.Cog):
             self.testing = json.load(f)
             
     @commands.command()
+    @commands.is_owner()
     async def testtest(self, ctx, wut, alsowut):
         self.testing[str(wut)] = str(alsowut)
         with open(r"commands/creating.json", "w") as f:
             json.dump(self.testing, f, indent=4)
+            
+    @commands.command()
+    @commands.is_owner()
+    async def teval(self, ctx, evl): 
+        try:
+            eval(evl)
+        except:
+            await ctx.send("Failed to run code")
+    
+    @commands.command()
+    @commands.is_owner()
+    async def reval(self, ctx, evl): 
+        try:
+            await ctx.send(str(eval(evl)))
+        except:
+            await ctx.send("Failed to run code")
         
 
     
