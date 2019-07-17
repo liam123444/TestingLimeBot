eaa382102c8995d054ff75f1388df875027b683b
FineLime <42850488+FineLime@users.noreply.github.com>
2019-07-17 19:40:57 +0100

    Update testing.py

--- commands/testing.py
+++ commands/testing.py
@@ -1,1 +1,26 @@
 import discord
+from discord.ext import commands
+from discord.ext.commands.cooldowns import BucketType
+import json
+import random
+
+class Testing(commands.Cog): 
+
+    def __init__(self, client):
+        self.client = client
+
+        with open(r"commands\creating.json", "r") as f: 
+            self.testing = json.load(f)
+            
+    @commands.command()
+    async def testtest(self, ctx, wut, alsowut):
+        self.testing[str(wut)] = str(alsowut)
+        with open(r"commands\creating.json", "w") as f:
+            json.dump(self.testing, f, indent=4)
+        
+
+    
+
+        
+def setup(client):
+    client.add_cog(Testing(client))
