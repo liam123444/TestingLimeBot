b65cbfda1b0c88c7e1888f05542f5054c407d0c2
FineLime <42850488+FineLime@users.noreply.github.com>
2019-07-19 18:11:29 +0100

    Update moderation.py

--- commands/moderation.py
+++ commands/moderation.py
@@ -12,8 +12,9 @@ class Moderation(commands.Cog):
     @commands.has_permissions(kick_members=True)
     async def kick(self, ctx, user : discord.Member, *, reason=None): 
         if ctx.author.top_role.position > user.top_role.position: 
+            await user.send(f"You have been kicked from **{ctx.guild.name}**\nReason:`{reason}`")
             await user.kick(reason=reason)
-            await ctx.send(f"{user.name} has been kicked by {ctx.author.name}\nReason:{reason}")
+            await ctx.send(f"**{user.name}** has been kicked by **{ctx.author.name}**\nReason:`{reason}`")
         else: 
             await ctx.send("You don't have permission to use that command")
             
@@ -21,8 +22,9 @@ class Moderation(commands.Cog):
     @commands.has_permissions(ban_members=True)
     async def ban(self, ctx, user : discord.Member, *, reason=None): 
         if ctx.author.top_role.position > user.top_role.position: 
+            await user.send(f"You have been banned from **{ctx.guild.name}**\nReason:`{reason}`")
             await user.ban(reason=reason)
-            await ctx.send(f"{user.name} has been banned by {ctx.author.name}\nReason:{reason}")
+            await ctx.send(f"**{user.name}** has been banned by **{ctx.author.name}**\nReason:`{reason}`")
         else: 
             await ctx.send("You don't have permission to use that command")        
         
