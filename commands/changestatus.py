4b3458b471e0f8af812d207e8c5fca04c8f039d9
FineLime <42850488+FineLime@users.noreply.github.com>
2019-07-19 18:04:55 +0100

    Update changestatus.py

--- commands/changestatus.py
+++ commands/changestatus.py
@@ -10,25 +10,25 @@ class Changestatus(commands.Cog):
     @commands.command()
     @commands.is_owner()
     async def watch(self, ctx, *, w): 
-        await client.change_presence(activity=discord.Activity(name=w, type=discord.ActivityType.watching))
+        await self.client.change_presence(activity=discord.Activity(name=w, type=discord.ActivityType.watching))
         await ctx.send(f"I am now watching {w}")
     
     @commands.command()
     @commands.is_owner()
     async def listen(self, ctx, *, l): 
-        await client.change_presence(activity=discord.Activity(name=l, type=discord.ActivityType.listening))
+        await self.client.change_presence(activity=discord.Activity(name=l, type=discord.ActivityType.listening))
         await ctx.send(f"I am now listening to {l}")
 
     @commands.command()
     @commands.is_owner()
     async def play(self, ctx, *, p): 
-        await client.change_presence(activity=discord.Activity(name=p, type=discord.ActivityType.playing))
+        await self.client.change_presence(activity=discord.Activity(name=p, type=discord.ActivityType.playing))
         await ctx.send(f"I am now playing {p}")
 
     @commands.command()
     @commands.is_owner()
     async def stream(self, ctx, *, s): 
-        await client.change_presence(activity=discord.Activity(name=s, type=discord.ActivityType.streaming))
+        await self.client.change_presence(activity=discord.Activity(name=s, type=discord.ActivityType.streaming))
         await ctx.send(f"I am now streaming {s}")
 
         
