1f88bc29ffceeac4606ee995afc644e2e448c7d3
FineLime <42850488+FineLime@users.noreply.github.com>
2019-07-19 18:03:41 +0100

    Update moderation.py

--- commands/moderation.py
+++ commands/moderation.py
@@ -11,8 +11,20 @@ class Moderation(commands.Cog):
     @commands.command()
     @commands.has_permissions(kick_members=True)
     async def kick(self, ctx, user : discord.Member, *, reason=None): 
-        await ctx.send(ctx.author.top_role.position)
-        await ctx.send(user.top_role.position)
+        if ctx.author.top_role.position > user.top_role.position: 
+            await user.kick(reason=reason)
+            await ctx.send(f"{user.name} has been kicked by {ctx.author.name}\nReason:{reason}")
+        else: 
+            await ctx.send("You don't have permission to use that command")
+            
+    @commands.command()
+    @commands.has_permissions(ban_members=True)
+    async def ban(self, ctx, user : discord.Member, *, reason=None): 
+        if ctx.author.top_role.position > user.top_role.position: 
+            await user.ban(reason=reason)
+            await ctx.send(f"{user.name} has been banned by {ctx.author.name}\nReason:{reason}")
+        else: 
+            await ctx.send("You don't have permission to use that command")        
         
         
 def setup(client):
