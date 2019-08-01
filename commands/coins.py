8aec604c4df583e0298651ccc71e0031763d1317
FineLime <42850488+FineLime@users.noreply.github.com>
2019-08-01 12:42:39 +0100

    Update coins.py

--- commands/coins.py
+++ commands/coins.py
@@ -14,8 +14,10 @@ class Coins(commands.Cog):
     async def addcoins(self, ctx, coins:int, u:str = "None"): 
         if u == "None":
             user = await self.client.pg_con.fetch("SELECT * FROM users WHERE id = $1", str(ctx.author.id))
+            name = ctx.author.name
         else:
             user = await self.client.pg_con.fetch("SELECT * FROM users WHERE id = $1", u[2:-1])
+            name = get(ctx.guild.members, id=str(u[2:-1])).name
 
         if not user: 
             await self.client.pg_con.execute("INSERT INTO users (id, coins) VALUES ($1, 0)", str(ctx.author.id))
@@ -32,7 +34,7 @@ class Coins(commands.Cog):
             await self.client.pg_con.execute("INSERT INTO users (id, coins) VALUES ($1, 0)", str(ctx.author.id))
 
         user = await self.client.pg_con.fetchrow("SELECT * FROM users WHERE id = $1", str(ctx.author.id))
-        await ctx.send(f"You have {user['coins']} coins!")
+        await ctx.send(f"{name} has been given {user['coins']} coins!")
 
 
 
