0a498858bba22a2c419df1255860a88cb3f482db
FineLime <42850488+FineLime@users.noreply.github.com>
2019-08-01 12:10:34 +0100

    Update rr.py

--- commands/rr.py
+++ commands/rr.py
@@ -15,17 +15,17 @@ class RR(commands.Cog):
     async def rr(self, ctx): 
         user = await self.client.pg_con.fetch("SELECT * FROM users WHERE id = $1", str(ctx.author.id))
         if not user: 
-            await self.client.pg_con.execute("INERT INTO users (id, coins) VALUES ($1, 0)", str(ctx.author.id))
+            await self.client.pg_con.execute("INSERT INTO users (id, coins) VALUES ($1, 0)", str(ctx.author.id))
             ctx.send("You don't have any coins to use this command.")
         user = user[0]
 
         if random.randint(1, 6) == 3: 
-            await ctx.send(f":gun: BANG! It was the bullet, I revived you but someone stole some of the cash while you were out.\n**(You lost{math.ceil(user['coins']/2)} coins)**")
-            await self.client.pg_con.execute("UPDATE users SET coins = $1 WHERE id=$2", math.ceil(user['coins']/2), str(ctx.author.id))    
+            await ctx.send(f":gun: BANG! It was the bullet, I revived you but someone stole some of the cash while you were out.\n**(You lost {math.ceil(user['coins']/2)} coins)**")
+            await self.client.pg_con.execute("UPDATE users SET coins = $1 WHERE id=$2", user['coins'] - math.ceil(user['coins']/2), str(ctx.author.id))    
 
         else:
-            await ctx.send(":gun: BANG! The gods want you to live another day, it was just a blank.")
-
+            await ctx.send(":gun: BANG! The gun shot out coins! It must hurt but I'm sure it's worth it!\n **(You gained {math.ceil(user['coins']/5)} coins**")
+            await self.client.pg_con.execute("UPDATE users SET coins = $1 WHERE id=$2", user['coins'] + math.ceil(user['coins']/5), str(ctx.author.id))
         
 def setup(client):
     client.add_cog(RR(client))
