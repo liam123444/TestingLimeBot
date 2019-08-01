2908cfbcdd31115c9d678f8253e4c2e881bbe069
FineLime <42850488+FineLime@users.noreply.github.com>
2019-08-01 11:55:59 +0100

    Update rr.py

--- commands/rr.py
+++ commands/rr.py
@@ -2,6 +2,8 @@ import discord
 from discord.ext import commands
 from discord.ext.commands.cooldowns import BucketType
 import random
+import asyncpg
+import math
 
 class RR(commands.Cog): 
 
@@ -11,8 +13,15 @@ class RR(commands.Cog):
     @commands.command(aliases=["roulette", "russianroulette"])
     @commands.cooldown(1, 10, BucketType.user)
     async def rr(self, ctx): 
+        user = await self.client.pg_con.fetch("SELECT * FROM users WHERE id = $1", str(ctx.author.id))
+        if not user: 
+            await self.client.pg_con.execute("INERT INTO users (id, coins) VALUES ($1, 0)", str(ctx.author.id))
+            ctx.send("You don't have any coins to use this command.")
+        user = user[0]
+
         if random.randint(1, 6) == 3: 
-            await ctx.send(":gun: BANG! Oh shit it was the bullet, let me cast my healing spell")
+            await ctx.send(f":gun: BANG! It was the bullet, I revived you but someone stole some of the cash while you were out.\n**(You lost{math.ceil(user["coins"]/2)} coins)**")
+            await self.client.pg_con.execute("UPDATE users SET coins = $1 WHERE id=$2", math.ceil(user['coins']/2), str(ctx.author.id))    
 
         else:
             await ctx.send(":gun: BANG! The gods want you to live another day, it was just a blank.")
