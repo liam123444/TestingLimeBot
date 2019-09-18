cd4528f356f615a5b52a99302c63f8947de7695f
FineLime <42850488+FineLime@users.noreply.github.com>
2019-09-18 21:15:07 +0100

    Update birthday.py

--- commands/birthday.py
+++ commands/birthday.py
@@ -38,7 +38,7 @@ class Bday(commands.Cog):
         bdays = sorted(bdays, key=lambda bdays: bdays['bday'])
         if bdays[0][1].strftime("%x") < datetime.datetime.now().strftime("%x"): 
             bdays[0][1] = datetime.datetime(int(datetime.datetime.now().strftime("%Y"))+1, int(bdays[0][1].strftime("%m")), int(bdays[0][1].strftime("%d")))
-            await self.client.pg_con.execute("UPDATE bdays SET bday = $1 WHERE ID = $2", f"{bdays[0][1].strftime('%Y')}-{bdays[0][1].strftime('%m')}-{bdays[0][1].strftime('%d')}", bdays[0][0]
+            await self.client.pg_con.execute("UPDATE bdays SET bday = $1 WHERE ID = $2", f"{bdays[0][1].strftime('%Y')}-{bdays[0][1].strftime('%m')}-{bdays[0][1].strftime('%d')}", bdays[0][0])
             bdays = sorted(bdays, key=lambda bdays: bdays['bday'])
             
         
