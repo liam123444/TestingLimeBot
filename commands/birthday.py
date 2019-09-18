019971e58489fb8bc39ec9ac8bd0ce2d266964e1
FineLime <42850488+FineLime@users.noreply.github.com>
2019-09-18 19:49:15 +0100

    Update birthday.py

--- commands/birthday.py
+++ commands/birthday.py
@@ -17,13 +17,22 @@ class Bday(commands.Cog):
             [461593347316776960, datetime.datetime(2019, 10, 25), "JB"], 
             [241650038776791041, datetime.datetime(2020, 6, 25), "Jason"], 
             [252416555785256960, datetime.datetime(2019, 12, 9), "William"], 
-            [282218272651345920, datetime.datetime(2019, 9, 23), "ssorsuper"]
+            [282218272651345920, datetime.datetime(2019, 9, 23), "ssorsuper"], 
+            [188344626543853568, datetime.datetime(2020, 7, 17), "scruffy"], 
+            [417626694166249484, datetime.datetime(2020, 4, 16), "Chao"], 
+            [234275916363202560, datetime.datetime(2020, 6, 25), "Dave"], 
+            [191283118752268288, datetime.datetime(2020, 3, 29), "Eggs"]
         ]
         bdays = sorted(bdays, key=lambda bdays: bdays[1])
         if bdays[0][1].strftime("%x") < datetime.datetime.now().strftime("%x"): 
             bdays[0][1] = datetime.datetime(int(datetime.datetime.now().strftime("%Y"))+1, int(bdays[0][1].strftime("%m")), int(bdays[0][1].strftime("%d")))
             bdays = sorted(bdays, key=lambda bdays: bdays[1])
-        await ctx.send(f"It is {bdays[0][2]}'s birthday in {(bdays[0][1]-datetime.datetime.now()).days+1} days")
+        
+        if bdays[0][1]-datetime.datetime.now()).days+1 == 0: 
+            embed = discord.Embed(title="Birthday :tada:", description=f"It is <@{bdays[0][0]}>'s birthday today! Say happy birthday! :tada:")
+        else: 
+            embed = discord.Embed(title="Next Birthday", description=f"It will be <@{bdays[0][0]}>'s birthday in {(bdays[0][1]-datetime.datetime.now()).days+1} days")
+        await ctx.send(embed=embed)
 
         
 def setup(client):
